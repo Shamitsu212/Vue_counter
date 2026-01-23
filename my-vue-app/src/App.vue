23b487b8a881a3e0e35780c9656cbf1c70a2ffcf
Shamitsu212 <darklin314@gmail.com>
2026-01-23 15:54:07 +0300

    feat: Пет проект сделан

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import Button from './components/Button.vue';
+
+import { ref } from 'vue';
+
+const counter = ref<number>(0)
+
+const incremet = () => {
+  counter.value++
+}
+
+const decrement = () => {
+  counter.value--
+}
+
+</script>
+
+<template>
+
+  <main class="Page">
+
+    <div class="Page__Content">
+
+      <p class="Content__Number">
+        {{counter}}
+      </p>
+
+      <div class="Content__ButtonContainer">
+
+        <Button color="red" @click="decrement"> Отнять </Button>
+        <Button color="green" @click="incremet"> Добавить </Button>
+
+      </div>
+
+    </div>
+
+  </main>
+  
+</template>
+
+<style scoped>
+.Page{
+  display: flex;
+  flex-direction: column;
+
+  width: 100vw;
+  height: 100vh;
+}
+.Page__Content{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 100%;
+}
+.Content__Number{
+  font-size: 6em;
+
+  font-weight: bold;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.Content__ButtonContainer{
+  margin-top: 5%;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  gap: 5%;
+
+  min-width: 300px;
+  min-height: 60px;
+}
+</style>
---
+++ my-vue-app/src/components/Button.vue
@@ -0,0 +1,93 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+type ButtonColor = 'green' | 'red'
+
+const props = defineProps<{
+  color?: ButtonColor
+}>()
+
+const colorClass = computed(() => props.color ?? 'green')
+const baseClass = computed(() => `${colorClass.value}Base`)
+
+const emit = defineEmits<{
+  (e: 'click'):void
+}>()
+
+</script>
+
+<template>
+
+    <div class="buttonWrapper">
+
+        <button class="button" :class="colorClass" @click="emit('click')">
+            <slot></slot>
+        </button>
+
+        <div class="baseClass" :class="baseClass"></div>
+    </div>
+  
+</template>
+
+<style scoped>
+.buttonWrapper {
+  display: inline-block;
+  position: relative;
+}
+
+.button {
+  padding: 10px 20px;
+
+  border-radius: 12px;
+
+  font-size: 1.5em;
+  font-weight: bold;
+  color: white;
+
+  border: none;
+  cursor: pointer;
+
+  position: relative;
+  z-index: 2;
+
+  transition: transform 0.1s;
+}
+
+
+.buttonBase {
+  position: absolute;
+
+  left: 0;
+  top: 73%;
+
+  width: 100%;
+  height: 17px; 
+
+  border-radius: 0px 0px 12px 12px;
+
+  z-index: 1;
+
+  transition: 0.1s;
+}
+
+
+.green {
+  background-color: #28a745;
+}
+.greenBase {
+  background-color: #218838;
+}
+
+
+.red {
+  background-color: #dc3545;
+}
+.redBase {
+  background-color: #c82333;
+}
+
+
+.buttonWrapper:hover .button {
+  transform: translateY(4.2px);
+}
+</style>
